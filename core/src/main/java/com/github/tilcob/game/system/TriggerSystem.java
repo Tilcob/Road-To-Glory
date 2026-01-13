82cf96a0b8713e7432b17d5ae4fc0002fd801309
Tilcob <jakobdorninger19@gmail.com>
2026-01-13 12:39:45 +0100

    Made a cache in trigger system.

--- core/src/main/java/com/github/tilcob/game/system/TriggerSystem.java
+++ core/src/main/java/com/github/tilcob/game/system/TriggerSystem.java
@@ -1,10 +1,13 @@
 package com.github.tilcob.game.system;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntityListener;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.IntMap;
 import com.github.tilcob.game.audio.AudioManager;
 import com.github.tilcob.game.component.Tiled;
 import com.github.tilcob.game.component.Trigger;
@@ -17,6 +20,18 @@ import java.util.EnumMap;
 public class TriggerSystem extends IteratingSystem implements Disposable {
     private final EnumMap<Trigger.Type, TriggerHandler> handlers;
     private final GameEventBus eventBus;
+    private final IntMap<Entity> entitiesByTiledId = new IntMap<>();
+    private final EntityListener tiledListener = new EntityListener() {
+        @Override
+        public void entityAdded(Entity entity) {
+            addEntityToIndex(entity);
+        }
+
+        @Override
+        public void entityRemoved(Entity entity) {
+            removeEntityFromIndex(entity);
+        }
+    };
 
     public TriggerSystem(AudioManager audioManager, GameEventBus eventBus) {
         super(Family.all(Trigger.class).get());
@@ -40,8 +55,7 @@ public class TriggerSystem extends IteratingSystem implements Disposable {
         if (handler == null) return;
 
         for (Entity triggeringEntity : trigger.getEntities()) {
-            Tiled tiled = Tiled.MAPPER.get(triggerEntity);
-            Entity entity = entityByTiledId(tiled.getId()) != null ? entityByTiledId(tiled.getId()) : triggerEntity;
+            Entity entity = resolveTriggerEntity(triggerEntity);
             handler.execute(entity, triggeringEntity);
         }
 
@@ -52,24 +66,64 @@ public class TriggerSystem extends IteratingSystem implements Disposable {
         Trigger trigger = Trigger.MAPPER.get(event.trigger());
         TriggerHandler handler = handlers.get(trigger.getType());
         if (handler == null) return;
-        Tiled tiled = Tiled.MAPPER.get(event.trigger());
-        Entity entity = entityByTiledId(tiled.getId()) != null ? entityByTiledId(tiled.getId()): event.trigger();
+        Entity entity = resolveTriggerEntity(event.trigger());
         handler.exit(entity, event.player());
     }
 
-    private Entity entityByTiledId(int id) {
-        if (id == 0)  return null;
-        ImmutableArray<Entity> entities = getEngine().getEntitiesFor(Family.all(Tiled.class).get());
+    @Override
+    public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
+        ImmutableArray<Entity> entities = engine.getEntitiesFor(Family.all(Tiled.class).get());
         for (Entity entity : entities) {
-            if (Tiled.MAPPER.get(entity).getId() == id) {
-                return entity;
-            }
+            addEntityToIndex(entity);
         }
-        return null;
+        engine.addEntityListener(Family.all(Tiled.class).get(), tiledListener);
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+        engine.removeEntityListener(tiledListener);
+        entitiesByTiledId.clear();
+        super.removedFromEngine(engine);
+    }
+
+    private Entity resolveTriggerEntity(Entity triggerEntity) {
+        Tiled tiled = Tiled.MAPPER.get(triggerEntity);
+        if (tiled == null) return triggerEntity;
+        Entity entity = entityByTiledId(tiled.getId());
+        return entity != null ? entity : triggerEntity;
+    }
+
+    private void addEntityToIndex(Entity entity) {
+        Tiled tiled = Tiled.MAPPER.get(entity);
+        if (tiled == null) return;
+        int id = tiled.getId();
+        if (id == 0) return;
+        entitiesByTiledId.put(id, entity);
+    }
+
+    private void removeEntityFromIndex(Entity entity) {
+        Tiled tiled = Tiled.MAPPER.get(entity);
+        if (tiled == null) return;
+        int id = tiled.getId();
+        if (id == 0) return;
+        if (entitiesByTiledId.get(id) == entity) {
+            entitiesByTiledId.remove(id);
+        }
+    }
+
+    private Entity entityByTiledId(int id) {
+        if (id == 0) return null;
+        return entitiesByTiledId.get(id);
     }
 
     @Override
     public void dispose() {
         eventBus.unsubscribe(ExitTriggerEvent.class, this::onExit);
+        Engine engine = getEngine();
+        if (engine != null) {
+            engine.removeEntityListener(tiledListener);
+        }
+        entitiesByTiledId.clear();
     }
 }
