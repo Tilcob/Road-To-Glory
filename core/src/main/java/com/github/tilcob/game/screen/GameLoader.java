6a51901be77bc342bcba61410c0ce9f808dac127
Tilcob <jakobdorninger19@gmail.com>
2026-01-15 20:52:04 +0100

    Added NPC-specific `.yarn` dialog files and integrated `YarnDialogLoader` into `GameLoader`. Updated `DialogAsset` to support NPC-specific file naming and retrieval.
    Signed-off-by: Tilcob <jakobdorninger19@gmail.com>

--- core/src/main/java/com/github/tilcob/game/screen/GameLoader.java
+++ core/src/main/java/com/github/tilcob/game/screen/GameLoader.java
@@ -3,17 +3,18 @@ package com.github.tilcob.game.screen;
 import com.github.tilcob.game.GameServices;
 import com.github.tilcob.game.assets.*;
 import com.github.tilcob.game.dialog.DialogLoader;
+import com.github.tilcob.game.dialog.YarnDialogLoader;
 import com.github.tilcob.game.quest.QuestFactory;
 
 public class GameLoader {
     private final GameServices services;
     private final QuestFactory questFactory;
-    private final DialogLoader dialogLoader;
+    private final YarnDialogLoader dialogLoader;
 
     public GameLoader(GameServices services) {
         this.services = services;
         this.questFactory = new QuestFactory(services.getEventBus());
-        this.dialogLoader = new DialogLoader();
+        this.dialogLoader = new YarnDialogLoader();
     }
 
     public void queueAll() {
@@ -27,7 +28,8 @@ public class GameLoader {
             services.getAllQuests().putAll(questFactory.loadAll(questAsset));
         }
         for (DialogAsset dialogAsset : DialogAsset.values()) {
-            services.getAllDialogs().putAll(dialogLoader.load(dialogAsset));
+            services.getAllDialogs().put(dialogAsset.getNpcName(),
+                dialogLoader.load(dialogAsset.getFileHandle()));
         }
         services.getAssetManager().queue(SkinAsset.DEFAULT);
     }
