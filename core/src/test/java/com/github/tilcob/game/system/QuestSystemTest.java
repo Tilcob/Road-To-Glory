267388fd18bbbcd490e29e5290f5546da1d93502
Tilcob <jakobdorninger19@gmail.com>
2026-01-19 23:52:30 +0100

    Wired the dialog maps in the test environment.
    Signed-off-by: Tilcob <jakobdorninger19@gmail.com>

--- core/src/test/java/com/github/tilcob/game/system/QuestSystemTest.java
+++ core/src/test/java/com/github/tilcob/game/system/QuestSystemTest.java
@@ -87,8 +87,8 @@ class QuestSystemTest extends HeadlessGdxTest {
         YarnDialogLoader dialogLoader = new YarnDialogLoader();
         DialogData dialogData = dialogLoader.load(Gdx.files.internal("tests/quests_test/start_node_flag_test.yarn"));
         Map<String, DialogData> questDialogs = Map.of("start_node_flag_test", dialogData);
-        QuestLifecycleService questLifecycleService = new QuestLifecycleService(eventBus, registry, questDialogs);
-        QuestYarnRuntime runtime = new QuestYarnRuntime(new QuestYarnBridge(questLifecycleService), Map.of(), Map.of());
+        QuestLifecycleService questLifecycleService = new QuestLifecycleService(eventBus, registry, Map.of());
+        QuestYarnRuntime runtime = new QuestYarnRuntime(new QuestYarnBridge(questLifecycleService), Map.of(), questDialogs);
         questLifecycleService.setQuestYarnRuntime(runtime);
         QuestSystem questSystem = new QuestSystem(eventBus, questLifecycleService);
         Engine engine = new Engine();
