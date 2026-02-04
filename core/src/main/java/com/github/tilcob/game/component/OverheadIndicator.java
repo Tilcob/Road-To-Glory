b71200bdb0e788cac640573a98f6f64a12a9d2f6
Tilcob <jakobdorninger19@gmail.com>
2026-02-04 18:11:50 +0100

    Added OverheadIndicatorType enum with baseline indicator identifiers for overhead markers.
    Added OverheadIndicator ECS component to store declarative overhead indicator data and mapper constant.
    
    Added OverheadIndicatorAnimation ECS component to hold runtime animation state and mapper constant.
    
    Signed-off-by: Tilcob <jakobdorninger19@gmail.com>

---
+++ core/src/main/java/com/github/tilcob/game/component/OverheadIndicator.java
@@ -0,0 +1,81 @@
+package com.github.tilcob.game.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+
+public class OverheadIndicator implements Component {
+    public static final ComponentMapper<OverheadIndicator> MAPPER = ComponentMapper.getFor(OverheadIndicator.class);
+
+    private final OverheadIndicatorType indicatorId;
+    private final Vector2 offset;
+    private float baseScale;
+    private Color color;
+    private boolean visible;
+    private Boolean allowPulse;
+    private Boolean allowBob;
+
+    public OverheadIndicator(OverheadIndicatorType indicatorId, Vector2 offset, float baseScale, Color color, boolean visible) {
+        this.indicatorId = indicatorId;
+        this.offset = offset;
+        this.baseScale = baseScale;
+        this.color = color;
+        this.visible = visible;
+    }
+
+    public OverheadIndicatorType getIndicatorId() {
+        return indicatorId;
+    }
+
+    public Vector2 getOffset() {
+        return offset;
+    }
+
+    public float getBaseScale() {
+        return baseScale;
+    }
+
+    public void setBaseScale(float baseScale) {
+        this.baseScale = baseScale;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public Boolean getAllowPulse() {
+        return allowPulse;
+    }
+
+    public void setAllowPulse(Boolean allowPulse) {
+        this.allowPulse = allowPulse;
+    }
+
+    public Boolean getAllowBob() {
+        return allowBob;
+    }
+
+    public void setAllowBob(Boolean allowBob) {
+        this.allowBob = allowBob;
+    }
+
+    public enum OverheadIndicatorType {
+        QUEST_AVAILABLE,
+        DANGER,
+        ANGRY,
+        INFO,
+    }
+}
