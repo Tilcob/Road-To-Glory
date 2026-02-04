23cac2d49769a2bd5cc889eb8c9b240d2e787f6f
Tilcob <jakobdorninger19@gmail.com>
2026-02-04 08:46:33 +0100

    ..
    Signed-off-by: Tilcob <jakobdorninger19@gmail.com>

--- core/src/main/java/com/github/tilcob/game/system/EquipmentStatModifierSystem.java
+++ core/src/main/java/com/github/tilcob/game/system/EquipmentStatModifierSystem.java
@@ -3,12 +3,14 @@ package com.github.tilcob.game.system;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.github.tilcob.game.component.Equipment;
 import com.github.tilcob.game.component.Item;
 import com.github.tilcob.game.component.StatModifierComponent;
 import com.github.tilcob.game.event.GameEventBus;
 import com.github.tilcob.game.event.StatRecalcEvent;
+import com.github.tilcob.game.event.UpdateEquipmentEvent;
 import com.github.tilcob.game.item.ItemCategory;
 import com.github.tilcob.game.item.ItemDefinition;
 import com.github.tilcob.game.item.ItemDefinitionRegistry;
@@ -20,7 +22,7 @@ import com.github.tilcob.game.stat.StatType;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EquipmentStatModifierSystem extends IteratingSystem {
+public class EquipmentStatModifierSystem extends IteratingSystem implements Disposable {
     private static final String ITEM_SOURCE_PREFIX = "item:";
     private static final Map<String, StatType> STAT_TYPE_LOOKUP = buildStatTypeLookup();
 
@@ -29,6 +31,8 @@ public class EquipmentStatModifierSystem extends IteratingSystem {
     public EquipmentStatModifierSystem(GameEventBus eventBus) {
         super(Family.all(Equipment.class, StatModifierComponent.class).get());
         this.eventBus = eventBus;
+
+        eventBus.subscribe(UpdateEquipmentEvent.class, this::onUpdateEquipment);
     }
 
     @Override
@@ -37,7 +41,10 @@ public class EquipmentStatModifierSystem extends IteratingSystem {
         StatModifierComponent modifiers = StatModifierComponent.MAPPER.get(entity);
         if (equipment == null || modifiers == null) return;
         if (!equipment.consumeDirty()) return;
+        recalcModifiers(entity, equipment, modifiers);
+    }
 
+    private void recalcModifiers(Entity entity, Equipment equipment, StatModifierComponent modifiers) {
         modifiers.removeModifiersBySourcePrefix(ITEM_SOURCE_PREFIX);
 
         for (ObjectMap.Entry<ItemCategory, Entity> entry : equipment.getEquippedSlots()) {
@@ -80,5 +87,25 @@ public class EquipmentStatModifierSystem extends IteratingSystem {
             return null;
         }
         return STAT_TYPE_LOOKUP.get(key.id());
+    }
+
+    private void onUpdateEquipment(UpdateEquipmentEvent event) {
+        if (event == null || event.player() == null) {
+            return;
+        }
+        Equipment equipment = Equipment.MAPPER.get(event.player());
+        StatModifierComponent modifiers = StatModifierComponent.MAPPER.get(event.player());
+        if (equipment == null || modifiers == null) {
+            return;
+        }
+        if (!equipment.consumeDirty()) {
+            return;
+        }
+        recalcModifiers(event.player(), equipment, modifiers);
+    }
+
+    @Override
+    public void dispose() {
+        eventBus.unsubscribe(UpdateEquipmentEvent.class, this::onUpdateEquipment);
     }
 }
